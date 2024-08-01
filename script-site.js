d92719c397f8cb86b875065086945ba9ad0d5412
GahSouzaDEV <emerdemolidor@gmail.com>
2024-08-01 20:50:08 -0300

    atualização link facebook

--- script-site.js
+++ script-site.js
@@ -8,7 +8,7 @@ window.onresize = function() {
 
 function facebook()
 {
-    window.open('//www.facebook.com/profile.php?id=61563240173560&mibextid=qi2Omg&rdid=EF5o2wO6p9OPtvWU&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2FzkLaKCcWPLB4TSA8%2F%3Fmibextid%3Dqi2Omg','_blank')
+    window.open('https://www.facebook.com/profile.php?id=61554782643114&mibextid=kFxxJD','_blank')
 }
 
 function instagran()
