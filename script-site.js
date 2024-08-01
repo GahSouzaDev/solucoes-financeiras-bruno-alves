dccfb4681000e25fdddc7ed96a00ed84098b28b1
GahSouzaDEV <emerdemolidor@gmail.com>
2024-08-01 20:18:43 -0300

    atualização

---
+++ script-site.js
@@ -0,0 +1,4 @@
+function facebook()
+{
+    window.open('//www.facebook.com/profile.php?id=61563240173560&mibextid=qi2Omg&rdid=EF5o2wO6p9OPtvWU&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2FzkLaKCcWPLB4TSA8%2F%3Fmibextid%3Dqi2Omg','_blank')
+}
