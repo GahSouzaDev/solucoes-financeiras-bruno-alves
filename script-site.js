c1df91ba5f7186842286041b3863ea83fd16eb79
GahSouzaDEV <emerdemolidor@gmail.com>
2024-08-01 20:46:24 -0300

    atualização js

--- script-site.js
+++ script-site.js
@@ -1,4 +1,22 @@
+window.onresize = function() {
+    if (window.innerWidth >= 500) {
+        window.location.href = 'pagina-principal.html';
+    } else {
+        window.location.href = 'index.html';
+    }
+};
+
 function facebook()
 {
     window.open('//www.facebook.com/profile.php?id=61563240173560&mibextid=qi2Omg&rdid=EF5o2wO6p9OPtvWU&share_url=https%3A%2F%2Fwww.facebook.com%2Fshare%2FzkLaKCcWPLB4TSA8%2F%3Fmibextid%3Dqi2Omg','_blank')
+}
+
+function instagran()
+{
+    window.open('//www.instagram.com/bruno.alves.marques?igsh=b2l3c28wY2g3bWJ3','_blank')
+}
+
+function whatsapp()
+{
+    window.open('//wa.me/message/OIQDSPEFPUIFN1','_blank')
 }
